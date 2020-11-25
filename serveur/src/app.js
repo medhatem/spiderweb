97db3578c0ce460ff8905a214292b4c7060362b1
jolg2102 <gabriel.jolicoeur@usherbrooke.ca>
2020-11-25 12:15:03 -0500

    added logging to file

--- serveur/src/app.js
+++ serveur/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const indexRouter = require("./routes/index");
 const urlsRouter = require("./routes/urls");
@@ -11,7 +13,13 @@ const crawlers_auth_mw = require("./routes/crawlers-auth-middleware");
 const app = express();
 app.use(cors());
 
-app.use(cors());
+// https://stackoverflow.com/questions/27230365/morgan-logger-express-js-writing-file-and-showing-in-console
+// :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"
+app.use(
+  logger("combined", {
+    stream: fs.createWriteStream(path.join(__dirname, "../access.log"), { flags: "a" }),
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
